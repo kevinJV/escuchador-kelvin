3ab04dbc3de0d2e90c23736a3fff3ee879d2b8c7
roberto14118927 <robertoruiz141189@gmail.com>
2019-03-01 14:45:47 -0600

    primer commit·

---
+++ santiago.js
@@ -0,0 +1,102 @@
+var express = require('express');
+var app = express();
+
+var net = require('net');
+var hex2ascii = require('hex2ascii');
+var mysql = require('mysql');
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var os = require('os');
+
+var interfaces = os.networkInterfaces();
+var addresses = [];
+for (var k in interfaces) {
+  for (var k2 in interfaces[k]) {
+    var address = interfaces[k][k2];
+    if (address.family === 'IPv4' && !address.internal) {
+      addresses.push(address.address);
+    }
+  }
+}
+
+
+var HOST = '134.209.76.81';
+var PORT = 4000;
+server.listen(5678);
+var arr;
+var arr1;
+var global_imei="";
+
+var sockets = [];
+var web_sockets = [];
+
+
+
+
+io.on('connection', function(socket) {
+      web_sockets.push(socket)
+      
+    socket.on('disconnect', function() {
+          var idx = web_sockets.indexOf(socket);
+          if (idx != -1) {
+            web_sockets.splice(idx, 1);
+          }
+    });
+
+    socket.on('end', function() {
+        
+    });
+
+    socket.on('error', function() {
+
+    });
+
+    socket.on('timeout', function() {
+        
+    });
+
+    socket.on('close', function() {
+        
+    });
+
+});
+
+io.on('error',function(err){ 
+  console.error(err)
+});
+
+
+
+net.createServer(function(sock) {
+    console.log('CONNECTED: ' + sock.remoteAddress +':'+ sock.remotePort);    
+    sock.on('data', function(data) {
+    });
+
+}).listen(PORT, HOST);
+
+
+function getCleanedString(cadena){
+   // Definimos los caracteres que queremos eliminar
+   var specialChars = "!@#$^&%*()+=[]\/{}|:<>?.";
+
+   // Los eliminamos todos
+   for (var i = 0; i < specialChars.length; i++) {
+       cadena= String(cadena).replace(new RegExp("\\" + specialChars[i], 'gi'), '');
+   }   
+
+   // Lo queremos devolver limpio en minusculas
+   cadena = cadena.toLowerCase();
+
+   // Quitamos espacios y los sustituimos por _ porque nos gusta mas asi
+   cadena = cadena.replace(/([\ \t]+(?=[\ \t])|^\s+|\s+$)/g,',');
+
+   // Quitamos acentos y "ñ". Fijate en que va sin comillas el primer parametro
+   cadena = cadena.replace(/á/gi,"a");
+   cadena = cadena.replace(/é/gi,"e");
+   cadena = cadena.replace(/í/gi,"i");
+   cadena = cadena.replace(/ó/gi,"o");
+   cadena = cadena.replace(/ú/gi,"u");
+   cadena = cadena.replace(/ñ/gi,"n");
+   return cadena;
+}
+
