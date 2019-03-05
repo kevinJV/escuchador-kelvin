a2c7809d9204f35ed13d45575054d4f933da9136
Kevin Jhair Velazquez Lopez <163199@ids.upchiapas.edu.mx>
2019-03-04 20:55:23 -0600

    Cambiado puerto al 5007. IO.algo ahora response

--- kelvin.js
+++ kelvin.js
@@ -22,81 +22,87 @@ for (var k in interfaces) {
 
 var HOST = '134.209.76.81';
 var PORT = 1007;
-server.listen(0007);
+server.listen(5007);
 var arr;
 var arr1;
-var global_imei="";
+var global_imei = "";
 
 var sockets = [];
 var web_sockets = [];
 
+var coordenadas = {
+  "latitude" : "1",
+  "longitude" : "2"
+};
 
 
+//Este es el chido (Aqui se conecta Angular)
+io.on('connection', function (socket) {
+  web_sockets.push(socket)
 
-io.on('connection', function(socket) {
-      web_sockets.push(socket)
-      
-    socket.on('disconnect', function() {
-          var idx = web_sockets.indexOf(socket);
-          if (idx != -1) {
-            web_sockets.splice(idx, 1);
-          }
-    });
+  socket.emit('coordenates', coordenadas); 
 
-    socket.on('end', function() {
-        
-    });
+  socket.on('disconnect', function () {
+    var idx = web_sockets.indexOf(socket);
+    if (idx != -1) {
+      web_sockets.splice(idx, 1);
+    }
+  });
 
-    socket.on('error', function() {
+  socket.on('end', function () {
 
-    });
+  });
 
-    socket.on('timeout', function() {
-        
-    });
+  socket.on('error', function () {
 
-    socket.on('close', function() {
-        
-    });
+  });
+
+  socket.on('timeout', function () {
+
+  });
+
+  socket.on('close', function () {
+
+  });
 
 });
 
-io.on('error',function(err){ 
+io.on('error', function (err) {
   console.error(err)
 });
 
 
-
-net.createServer(function(sock) {
-    console.log('CONNECTED: ' + sock.remoteAddress +':'+ sock.remotePort);    
-    sock.on('data', function(data) {
-    });
+//Esuchador (El chairo)
+net.createServer(function (sock) {
+  console.log('CONNECTED: ' + sock.remoteAddress + ':' + sock.remotePort);
+  sock.on('data', function (data) {
+  });
 
 }).listen(PORT, HOST);
 
 
-function getCleanedString(cadena){
-   // Definimos los caracteres que queremos eliminar
-   var specialChars = "!@#$^&%*()+=[]\/{}|:<>?.";
+function getCleanedString(cadena) {
+  // Definimos los caracteres que queremos eliminar
+  var specialChars = "!@#$^&%*()+=[]\/{}|:<>?.";
 
-   // Los eliminamos todos
-   for (var i = 0; i < specialChars.length; i++) {
-       cadena= String(cadena).replace(new RegExp("\\" + specialChars[i], 'gi'), '');
-   }   
+  // Los eliminamos todos
+  for (var i = 0; i < specialChars.length; i++) {
+    cadena = String(cadena).replace(new RegExp("\\" + specialChars[i], 'gi'), '');
+  }
 
-   // Lo queremos devolver limpio en minusculas
-   cadena = cadena.toLowerCase();
+  // Lo queremos devolver limpio en minusculas
+  cadena = cadena.toLowerCase();
 
-   // Quitamos espacios y los sustituimos por _ porque nos gusta mas asi
-   cadena = cadena.replace(/([\ \t]+(?=[\ \t])|^\s+|\s+$)/g,',');
+  // Quitamos espacios y los sustituimos por _ porque nos gusta mas asi
+  cadena = cadena.replace(/([\ \t]+(?=[\ \t])|^\s+|\s+$)/g, ',');
 
-   // Quitamos acentos y "ñ". Fijate en que va sin comillas el primer parametro
-   cadena = cadena.replace(/á/gi,"a");
-   cadena = cadena.replace(/é/gi,"e");
-   cadena = cadena.replace(/í/gi,"i");
-   cadena = cadena.replace(/ó/gi,"o");
-   cadena = cadena.replace(/ú/gi,"u");
-   cadena = cadena.replace(/ñ/gi,"n");
-   return cadena;
+  // Quitamos acentos y "ñ". Fijate en que va sin comillas el primer parametro
+  cadena = cadena.replace(/á/gi, "a");
+  cadena = cadena.replace(/é/gi, "e");
+  cadena = cadena.replace(/í/gi, "i");
+  cadena = cadena.replace(/ó/gi, "o");
+  cadena = cadena.replace(/ú/gi, "u");
+  cadena = cadena.replace(/ñ/gi, "n");
+  return cadena;
 }
 
